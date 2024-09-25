b5255e630c67357c477a95ec10eb28613b1dafa8
nkirukajane <36331903+nkirukajane@users.noreply.github.com>
2024-09-25 17:27:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function clock(){
+    var monthNames = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 
+        'September', 'Octorber', 'Novermber', 'December'];
+    var dayNames = ['Sunday', 'Monday', 'Tuseday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    var today = new Date();
+
+    document.getElementById('date').innerHTML = (dayNames[today.getDay()] + " " + today.getDate() + " " + monthNames[today.getMonth()] + " " + today.getFullYear());
+
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var day = h<11 ? 'AM' : 'PM';
+
+    h = h<10 ? '0' +h: h;
+    m = m<10 ? '0' +m: m;
+    s = s<10 ? '0' +s: s;
+
+    document.getElementById('hours').innerHTML = h;
+    document.getElementById('min').innerHTML = m;
+    document.getElementById('sec').innerHTML = s;
+} var inter = setInterval(clock,400);
